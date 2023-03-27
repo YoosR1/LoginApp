6459b4c9e2ba17878afe263354ea096865aeca67
Jorge Riera <joluriera@gmail.com>
2023-03-26 22:52:56 -0400

    add logo

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>UCFGO</Text>
+            <Image
+                style={styles.logo}
+                source={require('./assets/Logo.png')}
+            ></Image>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo: {
+        position: 'absolute',
+        alignSelf: 'center',
+        top: 50
+    }
 });
