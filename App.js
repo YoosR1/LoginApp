941da1d6d3cfc44674bad640465a272cd8982bf9
Jorge Riera <joluriera@gmail.com>
2023-03-27 17:09:51 -0400

    Login menu basic template

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Pressable } from 'react-native';
+
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>UCFGO</Text>
+            {/* <Text>UCFGO</Text> */}
             <Image
                 style={styles.logo}
                 source={require('./assets/Logo.png')}
             ></Image>
+
+            <View style={styles.loginForm}>
+                
+                <Text style={styles.inputBoxText}>
+                    Username:</Text>
+                <TextInput style={styles.inputBox}
+
+                />
+                <Text style={styles.inputBoxText}>
+                    Password:</Text>
+                <TextInput style={styles.inputBox}
+
+                />
+                <View style={styles.loginButton}>
+                    {/* todo: connect login api */}
+                    <Pressable /*onPress={onPressLogIn}*/>
+                        <Text style={styles.loginButtonText}>Confirm</Text>
+                    </Pressable> 
+                </View>
+            </View>
             <StatusBar style="auto" />
         </View>
     );
@@ -25,5 +47,41 @@ const styles = StyleSheet.create({
         position: 'absolute',
         alignSelf: 'center',
         top: 50
+    },
+    loginForm: {
+        minWidth: '80%',
+        // marginHorizontal: '10%',
+        marginVertical: '50%',
+        backgroundColor: '#f2bd00',
+        borderRadius: 30,
+        alignContent: 'center',
+        // justifyContent: 'center'
+    },
+    inputBox: {
+        height: 40,
+        marginHorizontal: 10,
+        marginBottom: 10,
+        padding: 10,
+        borderRadius: 30,
+        borderWidth: 1,
+        backgroundColor: '#D9D9D9'
+    },
+    inputBoxText: {
+        marginHorizontal: 10,
+        marginVertical: 5,
+        fontSize: 25,
+        fontWeight: '500'
+    },
+    loginButton: {
+        width: '50%',
+        margin: 10,
+        padding: 10,
+        alignSelf: 'center',
+        borderRadius: 30,
+        backgroundColor: '#D9D9D9'
+    },
+    loginButtonText: {
+        fontSize: 20,
+        fontWeight: '500'
     }
 });
