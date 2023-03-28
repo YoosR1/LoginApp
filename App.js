38af22d3212465f194f3997374c72b3ec0b627eb
Jorge Riera <joluriera@gmail.com>
2023-03-28 11:22:26 -0400

    added background

--- App.js
+++ App.js
@@ -1,36 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Pressable, ImageBackground } from 'react-native';
 
 
 export default function App() {
     return (
         <View style={styles.container}>
             {/* <Text>UCFGO</Text> */}
-            <Image
-                style={styles.logo}
-                source={require('./assets/Logo.png')}
-            ></Image>
+            <ImageBackground
+                style={styles.bgImage}
+                source={require('./assets/bgcrop.jpg')}>
+                <Image
+                    style={styles.logo}
+                    source={require('./assets/Logo.png')}
+                ></Image>
 
-            <View style={styles.loginForm}>
-                
-                <Text style={styles.inputBoxText}>
-                    Username:</Text>
-                <TextInput style={styles.inputBox}
+                <View style={styles.loginForm}>
+                    
+                    <Text style={styles.inputBoxText}>
+                        Username:</Text>
+                    <TextInput style={styles.inputBox}
 
-                />
-                <Text style={styles.inputBoxText}>
-                    Password:</Text>
-                <TextInput style={styles.inputBox}
+                    />
+                    <Text style={styles.inputBoxText}>
+                        Password:</Text>
+                    <TextInput style={styles.inputBox}
 
-                />
-                <View style={styles.loginButton}>
-                    {/* todo: connect login api */}
-                    <Pressable /*onPress={onPressLogIn}*/>
-                        <Text style={styles.loginButtonText}>Confirm</Text>
-                    </Pressable> 
+                    />
+                    <View style={styles.loginButton}>
+                        {/* todo: connect login api */}
+                        <Pressable /*onPress={onPressLogIn}*/>
+                            <Text style={styles.loginButtonText}>Confirm</Text>
+                        </Pressable> 
+                    </View>
                 </View>
-            </View>
+            </ImageBackground>
             <StatusBar style="auto" />
         </View>
     );
@@ -39,23 +43,29 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        // backgroundColor: '#fff',
+        // alignItems: 'center',
+        // justifyContent: 'center',
     },
     logo: {
         position: 'absolute',
+        // resizeMode: 'stretch',
         alignSelf: 'center',
         top: 50
     },
+    bgImage: {
+        objectFit: 'stretch',
+        flex: 1,
+        // justifyContent
+    },
     loginForm: {
         minWidth: '80%',
-        // marginHorizontal: '10%',
+        marginHorizontal: '10%',
         marginVertical: '50%',
         backgroundColor: '#f2bd00',
         borderRadius: 30,
         alignContent: 'center',
-        // justifyContent: 'center'
+        justifyContent: 'center'
     },
     inputBox: {
         height: 40,
@@ -73,10 +83,11 @@ const styles = StyleSheet.create({
         fontWeight: '500'
     },
     loginButton: {
-        width: '50%',
+        // width: '50%',
         margin: 10,
         padding: 10,
         alignSelf: 'center',
+        // justifyContent: 'center',
         borderRadius: 30,
         backgroundColor: '#D9D9D9'
     },
